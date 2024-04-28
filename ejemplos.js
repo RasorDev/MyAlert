d03924c128dd5df3699e7a317ad81f82c766fbd7
RasorDev <rasordev@proton.me>
2024-04-27 21:35:45 -0400

    Primer Readme

--- ejemplos.js
+++ ejemplos.js
@@ -5,9 +5,10 @@ var only_show = document.getElementById("only_show");
 only_show.addEventListener("click", function (e) {
   e.preventDefault();
   myalert.launch({    
-    title: "Probanding",
+    title: "MyAlert",
     icon: 'question',
-    iconcolor: '',
-    subtitle: 'Esto es un subtitulo'
+    //iconcolor: '#2b2b2b',
+    subtitle: 'Esto es un subtitulo personalizado.',
+    //msgs: ['mensaje 1', 'mensaje 2', 'mensaje 3']
   });
 });
